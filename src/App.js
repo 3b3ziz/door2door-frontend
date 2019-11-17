ef2e699b0d6294e35c2ff7d07b90af16f21f491c
Ahmad Abdelaziz <a.3b3ziz@gmail.com>
2019-11-17 05:33:42 +0200

    feat: intial clustering for vehicles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import GoogleMapReact from "google-map-react";
+import supercluster from "points-cluster";
 
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
 import * as firebase from "firebase/app";
 // Add the Firebase services that you want to use
 import "firebase/database";
+
+import carImage from "./icon.svg";
 
 var firebaseConfig = {
   // ...
@@ -16,22 +19,22 @@ var firebaseConfig = {
 // // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-const AnyReactComponent = ({ text, updatedAt }) => (
+const AnyReactComponent = ({ text, updatedAt, numPoints }) => (
   <div
-    title={updatedAt}
+    title={text}
     style={{
-      color: "white",
-      background: "grey",
-      padding: "15px 10px",
-      display: "inline-flex",
-      textAlign: "center",
-      alignItems: "center",
-      justifyContent: "center",
-      borderRadius: "100%",
-      transform: "translate(-50%, -50%)"
+      position: "absolute",
+      top: "50%",
+      left: "50%",
+      width: "18px",
+      height: "18px"
     }}
   >
-    {text}
+    {numPoints === 1 ? (
+      <img alt="a moving car" src={carImage} />
+    ): (
+      <div style={{ fontSize: 55 }}>{numPoints}</div>
+    )}
   </div>
 );
 
@@ -42,14 +45,47 @@ const door2doorOffice = {
 
 const locationsRef = firebase.database().ref("locations/");
 
-function App({ center = door2doorOffice, zoom = 16 }) {
+function App({ center = door2doorOffice, zoom = 13 }) {
+  const [initialVehicles, setInitialVehicles] = useState({});
   const [vehicles, setVehicles] = useState({});
   useEffect(() => {
     locationsRef.on("value", function(snapshot) {
       const vehiclesLocations = snapshot.val();
-      setVehicles(vehiclesLocations);
+      if (vehiclesLocations) {
+        setInitialVehicles(vehiclesLocations);
+        setVehicles(vehiclesLocations);
+      }
     });
   }, []);
+
+  const onChange = ({
+    bounds, zoom, center, marginBounds, size
+  }) => {
+    const v = Object.keys(initialVehicles).map(vehicle => {
+      const { lat, lng } = initialVehicles[vehicle];
+      return {
+        lat,
+        lng
+      };
+    });
+    const tmp = supercluster(v, {
+      minZoom: 3, // min zoom to generate clusters on
+      maxZoom: 15, // max zoom level to cluster the points on
+      radius: 60 // cluster radius in pixels
+    });
+    const tst = tmp({
+      bounds,
+      center,
+      zoom
+    });
+    const clusteredVehicles = tst.map(({ wx, wy, numPoints, points }) => ({
+      lat: wy,
+      lng: wx,
+      text: numPoints,
+      numPoints,
+    }))
+    setVehicles(clusteredVehicles);
+  };
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
@@ -57,15 +93,17 @@ function App({ center = door2doorOffice, zoom = 16 }) {
         bootstrapURLKeys={{ key: "AIzaSyDWukuXz2CdPeJh2GmUERJqw5z-qQu0It4" }}
         defaultCenter={center}
         defaultZoom={zoom}
+        onChange={onChange}
       >
         {Object.keys(vehicles).map(vehicle => {
-          const { lat, lng, at } = vehicles[vehicle];
+          const { lat, lng, at, numPoints = 1 } = vehicles[vehicle];
           return (
             <AnyReactComponent
               lat={lat}
               lng={lng}
               text={vehicle}
               updatedAt={at}
+              numPoints={numPoints}
             />
           );
         })}
